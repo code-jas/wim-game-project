48c506ae815ad7fedf5ec7e89fd3d3fbd282a166
kianlejano <91406104+kianlejano@users.noreply.github.com>
2023-04-11 11:13:04 +0800

    Created Modal.vue and test page

---
+++ components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="bg-gray-400 max-w-xl px-6 py-4 rounded-lg shadow-xl">
+        <!-- Header -->
+        <div class="flex mb-6"> 
+            <div class="basis-3/4">
+                <h1 class="italic font-bold text-5xl text-blue-v basis-3/4">About the game</h1>
+            </div>
+            <div class="basis-1/4 grid justify-items-end">
+                <button>
+                    <i class="uil uil-times text-xl text-lt-t-prim"></i>
+                </button>
+            </div>
+        </div>
+        <!-- Content -->
+        <div>
+            <div class="bg-white max-w-2xl px-14 py-3 rounded-lg border border-blue-v my-3.5">
+                <h1 class="text-center text-3xl font-bold mb-1">Image/Word Match</h1>
+                <p class="text-justify text-base">It is a type of visual puzzle that presents an image and provides a set of options for a corresponding word that best matches or describes the image.</p>
+            </div>
+            <div class="bg-white max-w-2xl px-14 py-3 rounded-lg border border-blue-v my-3.5">
+                <h1 class="text-center text-3xl font-bold mb-1">How to Play?</h1>
+                <p class="text-justify text-base">
+                    <ol>
+                        <li>1. Analyze the image presented by the game.</li>
+                        <li>2. Carefully read the different word options provided.</li>
+                        <li>3. Use the visual hint to narrow down the options.</li>
+                        <li>4. Select the word option that best describes the image.</li>
+                    </ol>
+                </p>
+            </div>
+            <div class="bg-white max-w-2xl px-14 py-3 rounded-lg border border-blue-v my-3.5">
+                <h1 class="text-center text-3xl font-bold mb-1">Difficulty</h1>
+                <p class="text-justify text-base">
+                    <ul>
+                        <li>Easy - provides an image and three (3) different options</li>
+                        <li>Medium - provides an image and four (4) different options</li>
+                        <li>Hard - provides an image and five (5) different options</li>
+                    </ul>
+                </p>
+            </div>
+            <div class="bg-white max-w-2xl px-16 py-3 rounded-lg border border-blue-v my-3.5">
+                <h1 class="text-center text-3xl font-bold mb-1">Mechanics</h1>
+                <p class="text-justify text-base">*calculations of score</p>
+            </div>
+            
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
