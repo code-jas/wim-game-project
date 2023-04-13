65a41026e3eb73389341334664c321825cf5b8fb
kianlejano <91406104+kianlejano@users.noreply.github.com>
2023-04-13 13:23:12 +0800

    Updated Difficulty.vue and LevelNumber.vue

--- components/gameboard/Difficulty.vue
+++ components/gameboard/Difficulty.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="bg-violet-l-alt h-screen flex items-center justify-center">
     <div class="flex items-center justify-center flex-col py-20 max-w-screen-2xl mx-auto">
-      <router-link to="/main">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="back-arrow"><g data-name="Layer 2"><g data-name="arrow-back"><rect width="24" height="24" opacity="0" transform="rotate(90 12 12)"></rect><path d="M19 11H7.14l3.63-4.36a1 1 0 1 0-1.54-1.28l-5 6a1.19 1.19 0 0 0-.09.15c0 .05 0 .08-.07.13A1 1 0 0 0 4 12a1 1 0 0 0 .07.36c0 .05 0 .08.07.13a1.19 1.19 0 0 0 .09.15l5 6A1 1 0 0 0 10 19a1 1 0 0 0 .64-.23 1 1 0 0 0 .13-1.41L7.14 13H19a1 1 0 0 0 0-2z"></path></g></g></svg> Back
-      </router-link>
+      <div class="flex justify-between w-[1500px]">
+        <router-link to="/main">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="back-arrow"><g data-name="Layer 2"><g data-name="arrow-back"><rect width="24" height="24" opacity="0" transform="rotate(90 12 12)"></rect><path d="M19 11H7.14l3.63-4.36a1 1 0 1 0-1.54-1.28l-5 6a1.19 1.19 0 0 0-.09.15c0 .05 0 .08-.07.13A1 1 0 0 0 4 12a1 1 0 0 0 .07.36c0 .05 0 .08.07.13a1.19 1.19 0 0 0 .09.15l5 6A1 1 0 0 0 10 19a1 1 0 0 0 .64-.23 1 1 0 0 0 .13-1.41L7.14 13H19a1 1 0 0 0 0-2z"></path></g></g></svg> Back
+        </router-link>
+        <div>
+          <button  type="button" class="w-[45px] h-[45px] flex items-center justify-center bg-violet-l-alt rounded-full   hover:scale-110 transition duration-300">
+            <i class="uil uil-question-circle text-blue text-4xl bg-violet-l-alt  rounded-full"></i>
+         </button>
+        </div>
+      </div>
       <div class="text-center font-black text-6xl text-lt-t-prim-alt">
         <h1>CHOOSE YOUR DIFFICULTY</h1>
       </div>
