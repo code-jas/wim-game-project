33c4cebfd5ad4b968a31246208a19b433e70572a
Gleronnelyn <128757984+Gleronnelyn@users.noreply.github.com>
2023-04-14 14:50:30 +0800

    Added Dark Mode for Dash Board

--- components/dashboard/DashboardNavbar.vue
+++ components/dashboard/DashboardNavbar.vue
@@ -1,11 +1,27 @@
-<template>
+<template> 
   <div class="flex  items-center justify-between w-10/12 h-[98px] bg-purple-950 px-9  mb-16 rounded-[15px] drop-shadow-lg">
-      <h1 class="font-franklin text-5xl text-white">Dashboard</h1>
-            
-      <div class="flex items-center justify-between gap-x-7">
-         <button  type="button" class="w-[45px] h-[45px] flex items-center justify-center bg-violet-l-alt rounded-full   hover:scale-110 transition duration-300">
-            <i class="uil uil-question-circle text-blue text-4xl bg-violet-l-alt  rounded-full"></i>
-         </button>
+      <h1 class="font-franklin text-5xl text-white">Dashboard</h1>  
+      
+      <div class="flex items-center justify-between gap-x-7">       
+         <div
+        class="w-[45px] h-[45px] flex items-center justify-center overflow-hidden rounded-full  hover:scale-110 transition duration-300"
+        :class="isDarkModeEnabled ? 'bg-dk-inp-alt hover:bg-dk-inp-alt' : 'bg-violet-l-alt hover:bg-violet-l-alt'"
+        @click="toggleDarkMode"
+         >
+            <i
+               :class="[
+                  'uil',
+                  'text-4xl',
+                  'hover:scale-105',
+                  'transition',
+                  'duration-200',
+                  isDarkModeEnabled
+                  ? 'uil-sun text-dk-t-prim bg-dk-inp-alt'
+                  : 'uil-moon text-blue bg-violet-l-alt'
+               ]"
+            ></i>
+         </div>
+         
          <button  @click="$emit('showModal')" type="button" class="w-[45px] h-[45px] flex items-center justify-center bg-violet-l-alt rounded-full   hover:scale-110 transition duration-300">
             <i class="uil uil-question-circle text-blue text-4xl bg-violet-l-alt  rounded-full"></i>
          </button>
@@ -13,12 +29,13 @@
             <i class="uil uil-plus text-2xl mr-2"></i> Add Player
          </button>
       </div>
-  </div>
+   </div>
+  
 </template>
 
 <script> 
 export default {
-
+  
 }
 </script>
 
